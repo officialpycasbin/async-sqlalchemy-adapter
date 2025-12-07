5628b21258310fe7eefa79e799550af8e527570b
Yang Luo <hsluoyz@gmail.com>
2025-12-07 22:28:05 +0800

    feat: add create_casbin_rule_model() API for Alembic migration support (#22)

--- casbin_async_sqlalchemy_adapter/adapter.py
+++ casbin_async_sqlalchemy_adapter/adapter.py
@@ -49,6 +49,36 @@ class CasbinRule(Base):
         return '<CasbinRule {}: "{}">'.format(self.id, str(self))
 
 
+def create_casbin_rule_model(base, table_name="casbin_rule"):
+    """Create a CasbinRule model using the given declarative base for Alembic integration."""
+
+    class CasbinRuleModel(base):
+        __tablename__ = table_name
+        __table_args__ = {"extend_existing": True}
+
+        id = Column(Integer, primary_key=True)
+        ptype = Column(String(255))
+        v0 = Column(String(255))
+        v1 = Column(String(255))
+        v2 = Column(String(255))
+        v3 = Column(String(255))
+        v4 = Column(String(255))
+        v5 = Column(String(255))
+
+        def __str__(self):
+            arr = [self.ptype]
+            for v in (self.v0, self.v1, self.v2, self.v3, self.v4, self.v5):
+                if v is None:
+                    break
+                arr.append(v)
+            return ", ".join(arr)
+
+        def __repr__(self):
+            return '<CasbinRule {}: "{}">'.format(self.id, str(self))
+
+    return CasbinRuleModel
+
+
 class Filter:
     ptype = []
     v0 = []
